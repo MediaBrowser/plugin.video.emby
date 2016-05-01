d8da6d0e089a14d5d634c35bef089a25112b2945
tomkat83 <kathriner.thomas@gmail.com>
2016-05-01 10:34:54 +0200

    Fix TypeError when trying to transcode
    - Fixes #19

--- resources/lib/playutils.py
+++ resources/lib/playutils.py
@@ -55,7 +55,7 @@ class PlayUtils():
             # Set playmethod property
             utils.window('emby_%s.playmethod' % playurl, "DirectStream")
 
-        elif self.isTranscoding():
+        else:
             self.logMsg("File is transcoding.", 1)
             quality = {
                 'maxVideoBitrate': self.getBitrate(),
@@ -214,13 +214,6 @@ class PlayUtils():
                     "required is: %s" % (settings, sourceBitrate), 1)
         if settings < sourceBitrate:
             return False
-        return True
-
-    def isTranscoding(self):
-        # Make sure the server supports it
-        if not self.item['MediaSources'][0]['SupportsTranscoding']:
-            return False
-
         return True
 
     def getBitrate(self):
