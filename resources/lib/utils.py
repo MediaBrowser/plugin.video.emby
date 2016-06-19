54c32adad6767364e1e3b80733efbc0b051ae08f
tomkat83 <kathriner.thomas@gmail.com>
2016-06-19 15:01:25 +0200

    Compatibility with latest Kodi Krypton
    - Fixes #56

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -353,7 +353,7 @@ def getKodiVideoDBPath():
         "14": 90,   # Helix
         "15": 93,   # Isengard
         "16": 99,   # Jarvis
-        "17": 104   # Krypton
+        "17": 106   # Krypton
     }
 
     dbPath = tryDecode(xbmc.translatePath(
