0aa7b01a49c8b5ed896e72cbb80064bbb035b442
angelblue05 <tamara.angel05@gmail.com>
2016-08-08 00:38:16 -0500

    Update login dialog

--- resources/lib/dialog/loginconnect.py
+++ resources/lib/dialog/loginconnect.py
@@ -2,16 +2,17 @@
 
 ##################################################################################################
 
-import hashlib
 import logging
 import os
 
 import xbmcgui
 import xbmcaddon
 
+from utils import language as lang
+
 ##################################################################################################
 
-log = logging.getLogger("EMBY."+__name__)
+log = logging.getLogger("EMBY."+__name__.split('.')[-1])
 addon = xbmcaddon.Addon('plugin.video.emby')
 
 ACTION_PARENT_DIR = 9
@@ -19,19 +20,34 @@ ACTION_PREVIOUS_MENU = 10
 ACTION_BACK = 92
 SIGN_IN = 200
 CANCEL = 201
+ERROR_TOGGLE = 202
+ERROR_MSG = 203
+ERROR = {
+    'Invalid': 1,
+    'Empty': 2
+}
 
 ##################################################################################################
 
 
 class LoginConnect(xbmcgui.WindowXMLDialog):
 
-    user = None
-    password = None
+    _user = None
+    isError = False
 
 
     def __init__(self, *args, **kwargs):
 
         xbmcgui.WindowXMLDialog.__init__(self, *args, **kwargs)
+
+    def setConnect(self, connect_manager):
+        self._connect_manager = connect_manager
+
+    def isLoggedIn(self):
+        return True if self._user else False
+
+    def getUser(self):
+        return self._user
 
     def onInit(self):
         
@@ -40,6 +56,8 @@ class LoginConnect(xbmcgui.WindowXMLDialog):
         self.password_field = self._add_editcontrol(685,470,40,500, password=1)
         self.signin_button = self.getControl(SIGN_IN)
         self.remind_button = self.getControl(CANCEL)
+        self.error_toggle = self.getControl(ERROR_TOGGLE)
+        self.error_msg = self.getControl(ERROR_MSG)
 
         self.user_field.controlUp(self.remind_button)
         self.user_field.controlDown(self.password_field)
@@ -52,16 +70,27 @@ class LoginConnect(xbmcgui.WindowXMLDialog):
 
         if control == SIGN_IN:
             # Sign in to emby connect
+            self._disable_error()
+
             self.user = self.user_field.getText()
             self.password = self.password_field.getText()
-            #self.password = hashlib.md5(self.password_field.getText()).hexdigest()
-            self.close()
+
+            if not self.user or not self.password:
+                # Display error
+                self._error(ERROR['Empty'], lang(30608))
+                log.error("Username or password cannot be null")
+            
+            elif self._login(self.user, self.password):
+                self.close()
 
         elif control == CANCEL:
             # Remind me later
             self.close()
 
     def onAction(self, action):
+
+        if self.isError == ERROR['Empty'] and self.user_field.getText() and self.password_field.getText():
+            self._disable_error()
 
         if action in (ACTION_BACK, ACTION_PARENT_DIR, ACTION_PREVIOUS_MENU):
             self.close()
@@ -83,3 +112,24 @@ class LoginConnect(xbmcgui.WindowXMLDialog):
 
         self.addControl(control)
         return control
+
+    def _login(self, username, password):
+
+        result = self._connect_manager.loginToConnect(username, password)
+        if result is False:
+            self._error(ERROR['Invalid'], lang(33009))
+            return False
+        else:
+            self._user = result
+            return True
+
+    def _error(self, state, message):
+
+        self.isError = state
+        self.error_msg.setLabel(message)
+        self.error_toggle.setVisibleCondition('True')
+
+    def _disable_error(self):
+
+        self.isError = False
+        self.error_toggle.setVisibleCondition('False')
