d039840c80c2a56cd9f0cd2cb2b44bd2cd9560b4
tomkat83 <kathriner.thomas@gmail.com>
2016-08-16 18:17:11 +0200

    New setting to lower #PMS items to cache
    - Should fix #91

--- resources/lib/librarysync.py
+++ resources/lib/librarysync.py
@@ -832,7 +832,8 @@ class LibrarySync(Thread):
         # Initiate threads
         self.logMsg("Starting sync threads", 1)
         getMetadataQueue = Queue.Queue()
-        processMetadataQueue = Queue.Queue(maxsize=100)
+        processMetadataQueue = Queue.Queue(
+            maxsize=int(utils.settings('xmlCacheLimit')))
         getMetadataLock = Lock()
         processMetadataLock = Lock()
         # To keep track
