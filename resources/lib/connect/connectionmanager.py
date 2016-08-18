4513f96d14e6933bed93f74f5123320c3c74c6d5
angelblue05 <tamara.angel05@gmail.com>
2016-08-17 23:40:25 -0500

    Reduce logging

--- resources/lib/connect/connectionmanager.py
+++ resources/lib/connect/connectionmanager.py
@@ -110,7 +110,7 @@ class ConnectionManager(object):
             servers.sort(key=lambda x: datetime.strptime(x['DateLastAccessed'], "%Y-%m-%dT%H:%M:%SZ"), reverse=True)
         except TypeError:
             servers.sort(key=lambda x: datetime(*(time.strptime(x['DateLastAccessed'], "%Y-%m-%dT%H:%M:%SZ")[0:6])), reverse=True)
-        log.info("Servers: %s" % servers)
+
         return servers[0]
 
     def _mergeServers(self, list1, list2):
