498dfb67d7aaf2115c96c89f736be16332d22027
tomkat83 <kathriner.thomas@gmail.com>
2017-03-31 16:43:05 +0200

    Remove obsolete import

--- resources/lib/itemtypes.py
+++ resources/lib/itemtypes.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from xbmc import sleep
 
 import artwork
-from utils import tryEncode, tryDecode, settings, window, kodiSQL, \
+from utils import tryEncode, tryDecode, window, kodiSQL, \
     CatchExceptions
 import plexdb_functions as plexdb
 import kodidb_functions as kodidb
