7ea2fbf41765d0c298bb2607ad8bb50e4df0295a
Croneter <kathriner.thomas@gmail.com>
2018-05-14 20:59:32 +0200

    Version bump

--- resources/lib/variables.py
+++ resources/lib/variables.py
@@ -76,7 +76,7 @@ COMPANION_PORT = int(_ADDON.getSetting('companionPort'))
 PKC_MACHINE_IDENTIFIER = None
 
 # Minimal PKC version needed for the Kodi database - otherwise need to recreate
-MIN_DB_VERSION = '2.0.23'
+MIN_DB_VERSION = '2.0.24'
 
 # Database paths
 _DB_VIDEO_VERSION = {
