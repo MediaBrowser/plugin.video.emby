ce284f668ae153c1f95eaf113ea82e123c0b9221
Croneter <kathriner.thomas@gmail.com>
2018-05-14 19:57:36 +0200

    Fix migration

--- resources/lib/migration.py
+++ resources/lib/migration.py
@@ -12,16 +12,13 @@ def check_migration():
     if last_migration == v.ADDON_VERSION:
         log.info('Already migrated to PKC version %s' % v.ADDON_VERSION)
         return
-    if not last_migration:
-        log.info('Never migrated, so checking everything')
-        last_migration = '1.0.0'
 
     if not compare_version(v.ADDON_VERSION, '1.8.2'):
         log.info('Migrating to version 1.8.1')
         # Set the new PKC theMovieDB key
         settings('themoviedbAPIKey', value='19c90103adb9e98f2172c6a6a3d85dc4')
 
-    if not compare_version(v.ADDON_VERSION, '2.0.24'):
+    if not compare_version(v.ADDON_VERSION, '2.0.25'):
         log.info('Migrating to version 2.0.24')
         # Need to re-connect with PMS to pick up on plex.direct URIs
         settings('ipaddress', value='')
