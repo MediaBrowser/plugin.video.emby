f7dfc2505899c939d17b8594fd7a0ef789e83560
dazedcrazy <komplex@live.ie>
2018-05-17 04:01:40 +0100

    contextmenu changes

--- resources/lib/context_entry.py
+++ resources/lib/context_entry.py
@@ -6,10 +6,10 @@ from xbmcaddon import Addon
 import xbmc
 import xbmcplugin
 import xbmcgui
+import context
 
 import plexdb_functions as plexdb
 from utils import window, settings, dialog, language as lang
-from dialogs import context
 import PlexFunctions as PF
 from PlexAPI import API
 import playqueue as PQ
@@ -96,18 +96,7 @@ class ContextMenu(object):
             options.append(OPTIONS['PMS_Play'])
         if self.kodi_type in v.KODI_VIDEOTYPES:
             options.append(OPTIONS['Transcode'])
-        # userdata = self.api.userdata()
-        # if userdata['Favorite']:
-        #     # Remove from emby favourites
-        #     options.append(OPTIONS['RemoveFav'])
-        # else:
-        #     # Add to emby favourites
-        #     options.append(OPTIONS['AddFav'])
-        # if self.kodi_type == "song":
-        #     # Set custom song rating
-        #     options.append(OPTIONS['RateSong'])
-        # Refresh item
-        # options.append(OPTIONS['Refresh'])
+
         # Delete item, only if the Plex Home main user is logged in
         if (window('plex_restricteduser') != 'true' and
                 window('plex_allows_mediaDeletion') == 'true'):
@@ -137,14 +126,7 @@ class ContextMenu(object):
             self._PMS_play()
         elif selected == OPTIONS['Extras']:
             self._extras()
-        # elif selected == OPTIONS['Refresh']:
-        #     self.emby.refreshItem(self.item_id)
-        # elif selected == OPTIONS['AddFav']:
-        #     self.emby.updateUserRating(self.item_id, favourite=True)
-        # elif selected == OPTIONS['RemoveFav']:
-        #     self.emby.updateUserRating(self.item_id, favourite=False)
-        # elif selected == OPTIONS['RateSong']:
-        #     self._rate_song()
+
         elif selected == OPTIONS['Addon']:
             xbmc.executebuiltin(
                 'Addon.OpenSettings(plugin.video.plexkodiconnect)')
