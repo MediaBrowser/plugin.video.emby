f00b5fe59d2cc72f78b53d900a68986e7b687bce
croneter <kathriner.thomas@gmail.com>
2018-05-20 14:22:05 +0200

    Revert "Fix ValueError if plex.tv returns Plex Cloud URIs"
    This reverts commit a7702573ad41b6db72469defb080119af6f73c0b.

--- resources/lib/PlexFunctions.py
+++ resources/lib/PlexFunctions.py
@@ -413,12 +413,7 @@ def _poke_pms(pms, queue):
                                              data['address'],
                                              data['port'])
         pms['connections'].insert(1, conn)
-    try:
-        protocol, address, port = url.split(':', 2)
-    except ValueError:
-        # e.g. .ork.plex.services uri, thanks Plex
-        protocol, address = url.split(':', 1)
-        port = data['port']
+    protocol, address, port = url.split(':', 2)
     address = address.replace('/', '')
     xml = DU().downloadUrl('%s/identity' % url,
                            authenticate=False,
