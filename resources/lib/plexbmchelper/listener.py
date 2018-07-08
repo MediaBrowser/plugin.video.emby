baebd1182554c782378fff1ed6e12315b16b0e5e
croneter <kathriner.thomas@gmail.com>
2018-07-08 12:28:28 +0200

    Fix Plex Companion crashing when connected to Plex Web

--- resources/lib/plexbmchelper/listener.py
+++ resources/lib/plexbmchelper/listener.py
@@ -145,7 +145,7 @@ class MyHandler(BaseHTTPRequestHandler):
             tracker.pop(0)
             msg = sub_mgr.msg(js.get_players()).format(
                 command_id=params.get('commandID', 0))
-            if sub_mgr.isplaying:
+            if sub_mgr.location == 'fullScreenVideo':
                 self.response(
                     msg,
                     {
