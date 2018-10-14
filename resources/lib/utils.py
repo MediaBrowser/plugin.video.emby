8199c2ce5b69184bef131e13780b206679e14f82
croneter <kathriner.thomas@gmail.com>
2018-10-14 11:12:48 +0200

    Fix PEP-8

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -619,14 +619,14 @@ def indent(elem, level=0):
     """
     Prettifies xml trees. Pass the etree root in
     """
-    i = "\n" + level*"  "
+    i = "\n" + level * "  "
     if len(elem):
         if not elem.text or not elem.text.strip():
             elem.text = i + "  "
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
         for elem in elem:
-            indent(elem, level+1)
+            indent(elem, level + 1)
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
     else:
