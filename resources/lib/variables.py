3bc1caebca5a6ea2e2e218c9bdfdfd004d8ef246
croneter <kathriner.thomas@gmail.com>
2018-10-24 17:23:38 +0200

    Require database reset

--- resources/lib/variables.py
+++ resources/lib/variables.py
@@ -84,7 +84,7 @@ COMPANION_PORT = int(_ADDON.getSetting('companionPort'))
 PKC_MACHINE_IDENTIFIER = None
 
 # Minimal PKC version needed for the Kodi database - otherwise need to recreate
-MIN_DB_VERSION = '2.0.27'
+MIN_DB_VERSION = '2.5.0'
 
 # Database paths
 DB_VIDEO_VERSION = {
