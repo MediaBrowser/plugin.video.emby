be45d914d32682163c9965aae38bcb8a483f9bdf
croneter <kathriner.thomas@gmail.com>
2018-11-10 16:09:06 +0100

    Wrap Kodi DB transactions explicitly

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -475,7 +475,7 @@ def unix_timestamp(seconds_into_the_future=None):
     return int((future - EPOCH).total_seconds())
 
 
-def kodi_sql(media_type=None):
+def kodi_sql(media_type=None, writer=False):
     """
     Open a connection to the Kodi database.
         media_type: 'video' (standard if not passed), 'plex', 'music', 'texture'
@@ -488,8 +488,13 @@ def kodi_sql(media_type=None):
         db_path = v.DB_TEXTURE_PATH
     else:
         db_path = v.DB_VIDEO_PATH
-    conn = connect(db_path, timeout=5.0)
-    conn.execute('PRAGMA journal_mode=WAL')
+    if writer:
+        conn = connect(db_path, timeout=5.0, isolation_level=None)
+        conn.execute('PRAGMA journal_mode=WAL')
+        # conn.execute('BEGIN TRANSACTION')
+    else:
+        conn = connect(db_path, timeout=5.0)
+        conn.execute('PRAGMA journal_mode=WAL')
     return conn
 
 
