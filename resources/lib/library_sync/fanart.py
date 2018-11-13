0c9e544e9e134cd0195ff808c5df590d63f9d66a
croneter <kathriner.thomas@gmail.com>
2018-11-13 14:48:25 +0100

    Suspend fanart sync if another sync is running

--- resources/lib/library_sync/fanart.py
+++ resources/lib/library_sync/fanart.py
@@ -31,7 +31,7 @@ class FanartThread(backgroundthread.KillableThread):
         return state.STOP_PKC
 
     def isSuspended(self):
-        return state.SUSPEND_LIBRARY_THREAD or state.STOP_SYNC
+        return state.SUSPEND_LIBRARY_THREAD or state.STOP_SYNC or state.DB_SCAN
 
     def run(self):
         try:
