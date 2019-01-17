01d23ea3a689ba84c8219006bf68d50178250f5d
angelblue05 <angelblue.dev@gmail.com>
2019-01-17 02:09:54 -0600

    Fix music videos

--- objects/musicvideos.py
+++ objects/musicvideos.py
@@ -187,7 +187,7 @@ class MusicVideos(KodiDb):
             obj['Path'] = "http://127.0.0.1:57578/emby/kodi/musicvideos/"
             params = {
                 'Name': obj['Filename'].encode('utf-8'),
-                'KodiId': bj['MvideoId'],
+                'KodiId': obj['MvideoId'],
                 'Id': obj['Id']
             }
             obj['Filename'] = "%s/file.strm?%s" % (obj['Id'], urllib.urlencode(params))
