b86a6549acc0fb1547723f3bd15c0dc415c221fc
croneter <kathriner.thomas@gmail.com>
2019-01-17 17:36:26 +0100

    Refresh Kodi view only once on full syncs

--- resources/lib/library_sync/full_sync.py
+++ resources/lib/library_sync/full_sync.py
@@ -403,7 +403,6 @@ class FullSync(common.fullsync_mixin):
                 self.successful = False
                 return
         finally:
-            common.update_kodi_library(video=True, music=True)
             if self.dialog:
                 self.dialog.close()
             if self.threader:
