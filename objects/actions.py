e9afcd475602519d1d87b93c52278284d5b1291d
Sebastian S <su4lfred@gmail.com>
2019-01-19 13:43:33 +0100

    Clear playlist if the user manually plays another item without stopping the player before

--- objects/actions.py
+++ objects/actions.py
@@ -46,9 +46,27 @@ class Actions(object):
 
     def detect_playlist(self, item):
 
+        ''' Sometimes it's required to clear the playlist to get everything working together.
+            Otherwise "Play from here" and cinema mode is going to break.
+        '''
         window('emby.context.widget', clear=True)
         xbmc.sleep(50)
-        if not xbmc.getCondVisibility('Integer.IsGreater(Playlist.Length(video),1)') and not item['Type'] == 'Audio':
+
+        ''' Do not clear playlist for audio items at all
+        '''
+        if item['Type'] == 'Audio':
+            return False
+
+        ''' Clear the playlist if the user starts a new playback from the GUI and there is still
+            an existing one set.
+        '''
+        if xbmc.getCondVisibility('Player.HasMedia + !Window.IsVisible(fullscreenvideo) + Integer.IsGreaterOrEqual(Playlist.Length(video),1)'):
+            return True
+
+        ''' Clear the pseudo-playlist that has been created by the library windows for a single
+            video item. This is required to get the cinema mode working.
+        '''
+        if not xbmc.getCondVisibility('Integer.IsGreater(Playlist.Length(video),1)'):
             return True
 
         return False
