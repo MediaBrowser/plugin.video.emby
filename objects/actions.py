9c541da8d318b6f55443df482505fc4eb1d71e12
angelblue05 <angelblue.dev@gmail.com>
2019-02-28 20:50:54 -0600

    Prevent division error
    If runtime is not correctly detected

--- objects/actions.py
+++ objects/actions.py
@@ -501,7 +501,7 @@ class Actions(object):
             listitem.setProperty('IsPlayable', 'true')
             listitem.setProperty('IsFolder', 'false')
 
-            if obj['Resume'] and seektime != False:
+            if obj['Resume'] and obj['Runtime'] and seektime != False:
 
                 listitem.setProperty('resumetime', str(obj['Resume']))
                 listitem.setProperty('StartPercent', str(((obj['Resume']/obj['Runtime']) * 100)))
