15c4da46a985ebe5d6043bf362c9312a5ee109da
croneter <croneter@users.noreply.github.com>
2019-04-14 14:49:44 +0200

    Reenable setting: recently added: don't show watched movies

--- resources/lib/library_sync/nodes.py
+++ resources/lib/library_sync/nodes.py
@@ -252,9 +252,13 @@ def node_recent(section, node_name):
     rule = etree.SubElement(xml, 'rule', attrib={'field': 'tag',
                                                  'operator': 'is'})
     etree.SubElement(rule, 'value').text = section.name
-    if (section.section_type == v.PLEX_TYPE_SHOW and
-            utils.settings('TVShowWatched') == 'false'):
+    if ((section.section_type == v.PLEX_TYPE_SHOW and
+            utils.settings('TVShowWatched') == 'false') or
+        (section.section_type == v.PLEX_TYPE_MOVIE and
+            utils.settings('MovieShowWatched') == 'false')):
         # Adds an additional rule if user deactivated the PKC setting
+        # "Recently Added: Also show already watched episodes"
+        # or
         # "Recently Added: Also show already watched episodes"
         rule = etree.SubElement(xml, 'rule', attrib={'field': 'playcount',
                                                      'operator': 'is'})
