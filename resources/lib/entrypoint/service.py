2e83ea94858ac136fc7e110e86b10f92faa5de62
angelblue05 <angelblue.dev@gmail.com>
2019-06-28 07:11:23 -0500

    Add to clear window prop

--- resources/lib/entrypoint/service.py
+++ resources/lib/entrypoint/service.py
@@ -463,7 +463,7 @@ class Service(xbmc.Monitor):
         properties = [
             "emby.play", "emby.autoplay", "emby_online", "emby.connected", "emby.resume",
             "emby.updatewidgets", "emby.external", "emby.external_check", "emby_deviceId",
-            "emby_pathverified", "emby_sync", "emby.restart", "emby.sync.pause"
+            "emby_pathverified", "emby_sync", "emby.restart", "emby.sync.pause", "emby.playlist.clear"
         ]
         for prop in properties:
             window(prop, clear=True)
