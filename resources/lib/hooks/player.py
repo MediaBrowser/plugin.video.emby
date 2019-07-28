d6a59194e24b9e84fa92beb2741c752ed9059bef
angelblue05 <angelblue.dev@gmail.com>
2019-07-28 03:09:15 -0500

    Adjust playback
    Use source runtime, add source id to playbackinfo, replace TheVoid

--- resources/lib/hooks/player.py
+++ resources/lib/hooks/player.py
@@ -487,7 +487,7 @@ class Player(xbmc.Player):
 
         ''' Stop all playback. Check for external player for positionticks.
         '''
-        LOG.info("[ played info ] %s", self.played)
+        LOG.debug("[ played info ] %s", self.played)
 
         for file in self.played:
 
@@ -499,6 +499,7 @@ class Player(xbmc.Player):
 
                     continue
 
+                LOG.info("[ played info ] %s", item)
                 item['Track'] = False
                 self.played[file] = item
                 window('emby.skip.%s.bool' % item['Id'], True)
