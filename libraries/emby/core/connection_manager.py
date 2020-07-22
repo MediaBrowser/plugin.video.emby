e5be30e05b6063955859edde15e56db0b398225f
Luke <luke.pulverenti@gmail.com>
2020-07-21 23:33:58 -0400

    Update connection_manager.py

--- libraries/emby/core/connection_manager.py
+++ libraries/emby/core/connection_manager.py
@@ -200,7 +200,7 @@ class ConnectionManager(object):
                 'url': self.get_emby_url(server, "Users/AuthenticateByName"),
                 'json': {
                     'username': username,
-                    'password': hashlib.sha1(password or "").hexdigest(),
+                    'pw': password or "",
                 }
             }
             if clear:
